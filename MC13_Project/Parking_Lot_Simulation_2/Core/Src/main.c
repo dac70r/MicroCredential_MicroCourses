033ab466d38dba6d93b86efe43df6f55c14bc0b5
Dennis <denniswong178@gmail.com>
2025-05-01 14:30:04 +0800

    Use Parking Sim 2

---
+++ MC13_Project/Parking_Lot_Simulation_2/Core/Src/main.c
@@ -0,0 +1,229 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+volatile uint32_t parking_space_remain = 200;
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  // Plan:
+  // 1. Use Pins PC0 as Sensor for Exit
+  // 2. Use Pins PC1 as Sensor for Entrance
+  // 3. Use Pins PB2 as Output Pin to Drive Exit LED
+  // 4. Use Pins PB3 as Output Pin to Drive Entrance LED
+
+  	RCC -> AHB1ENR  = 0x04;						// Configure Port C
+  	RCC -> AHB1ENR |= 0x02;						// Configure Port B
+
+	//RCC-> AHB1ENR = 0x40;						// Configure Port G, Port B, Port A, and Port C
+	//GPIOG -> MODER |= 0x24000000;				// Configure PG13 and PG14 as output
+  	//GPIOG -> ODR |= 0x00002000;				// On board LED PG13 - testing
+  	//GPIOG -> ODR |= 0x00004000;				// On board LED PG14 - testing
+
+	GPIOC -> MODER &= 0xFFFFFFF0;				// Mask the irrelevant pins
+	GPIOC -> MODER |= 0x00000000;				// Configure PC0 and PC1 as input pins
+
+	GPIOB -> MODER &= 0xFFFFFF0F;				// Mask the irrelevant pins
+	GPIOB -> MODER |= 0x00000050;				// Configure PB2 and PB3 as output pins
+	GPIOB -> PUPDR |= 0x000000A0; 				// Pull down PB2 and PB3 (Optional)
+
+	//EXTI ->
+	//NVIC_EnableIRQ(EXTI15_10_IIRQn);
+	// Sensors are active low: If Input States are HIGH -> NO DETECTION
+	// If Input States are HIGH: Exit/ Entrance LEDs are not lit
+  while (1)
+  {
+	if((GPIOC -> IDR & (1<<0)) == 0b00000000)
+	{
+		GPIOB->ODR |= (1 << 2);
+		// If exit == 1
+		parking_space_remain --;
+		//HAL_Delay(10);
+	}
+	else
+	{
+		GPIOB->ODR &= (0 << 2);
+	}
+
+	if((GPIOC -> IDR & (1<<1)) == 0b00000000)
+	{
+		GPIOB->ODR |= (1 << 3);
+		parking_space_remain ++;
+		//HAL_Delay(10);
+	}
+	else
+	{
+		GPIOB->ODR &= (0 << 3);
+	}
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
